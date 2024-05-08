32d9998d1c810d11a3b37096afec21bedf1299aa
gunner26735 <sidshukla2016@gmail.com>
2024-05-08 23:14:38 +0530

    added record donation method & new fetch donation records component

---
+++ src/components/orgs/TransactionList.js
@@ -0,0 +1,46 @@
+"use client";
+
+import { orgABI, contractAddress } from "@/app/provider";
+import { useReadContract } from "wagmi";
+
+export default function TransactionList(_orgAddress) {
+
+    const { data: orgTrasactionData, isFetched: orgTransactionDataFetched } = useReadContract({
+        abi: orgABI,
+        address: contractAddress,
+        functionName: 'listDonations',
+        args: [_orgAddress._orgAddress],
+    });
+    console.log(_orgAddress._orgAddress);
+    if (orgTransactionDataFetched) {
+        console.log(orgTrasactionData);
+        return (
+            <table className="text-center">
+                <thead>
+                    <tr className="border-4 border-solid border-l-0 border-r-0 text-xl">
+                        <th className="text-md px-6 py-3">Donor Address</th>
+                        <th className="text-md px-6 py-3">Transaction Hash</th>
+                        <th className="text-md px-6 py-3">Donoation Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orgTrasactionData.map((orgTx) =>(
+                        <tr>
+                            <td className="text-md px-6 py-3">{orgTx['donor_addr']}</td>
+                            <td className="text-md px-6 py-3">{orgTx['txhash']}</td>
+                            <td className="text-md px-6 py-3">{parseInt(orgTx['donation'])}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        );
+    }
+    else{
+        return(
+            <div>
+                loading.......
+            </div>
+        );
+    }
+}
